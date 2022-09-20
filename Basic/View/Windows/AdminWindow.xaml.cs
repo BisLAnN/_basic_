70f35865dc03192dacbe2741df2056eb33a02c00
user09 <harchenko.a0610>
2022-09-20 14:47:48 +0300

    first commit

---
+++ Basic/View/Windows/AdminWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Basic.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Basic.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadSomePage_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new UsersPage());
+        }
+
+        private void btnLoadUsers_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new SomePage());
+        }
+    }
+}
